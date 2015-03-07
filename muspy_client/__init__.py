3d7fc30e6a09b43eb33cee8866001ab8b66b3695
David Poisl <david@poisl.at>
2015-03-07 01:02:37 +0100

    rewrote in a saner way, updated to new api calls
    this now looks somewhat useable. still much work to do regarding
    documentation, a nicer representation of the API objects, etc.

--- muspy_client/__init__.py
+++ muspy_client/__init__.py
@@ -1,85 +1,175 @@
 """
-client for muspy.com
+OOP client for muspy.com
 """
+
 
 __author__ = 'David Poisl <david@poisl.at>'
 __version__ = '1.0.0'
 
+
 from . import api
 
 
+# TODO: docstrings
+# TODO: nice representation
+# TODO: testing
+
+
 class User(object):
-    def __init__(self, email, userid, notify, auth=None, **kwargs):
+    """
+    muspy.com user
+
+    :ivar str email: E-Mail Address
+    :ivar str password: user password
+    :ivar str userid: muspy.com user ID
+    :ivar bool notify: notification per mail enabled
+    :ivar bool notify_album: receive notifications for new albums
+    :ivar bool notify_single: receive notifications for new singles
+    :ivar bool notify_ep: receive notifications for new EPs
+    :ivar bool notify_live: receive notifications for live albums
+    :ivar bool notify_remix: receive notifications for remix releases
+    :ivar bool notify_other: receive notifications for other stuff
+    :ivar bool notify_compilation: receive notification for compilations
+    """
+    _fields = ('notify', 'notify_album', 'notify_single',
+               'notify_ep', 'notify_live', 'notify_compilation',
+               'notify_remix', 'notify_other', 'notify_compilation')
+
+    @property
+    def auth(self):
+        """
+        property for authentication data
+
+        :return: tuple of (email/username, password)
+        :rtype: tuple
+        """
+        return self.email, self.password
+
+    def __init__(self, email, password):
+        """
+        constructor
+
+        :param str email: email address for authentication
+        :param str password: password
+        """
         self.email = email
-        self.userid = userid
-        self.notify = notify
-        self.notifications = {k: v for (k, v) in kwargs.items()
-                              if k.startswith("notify_")}
-        self._artists = None
-        self._auth = auth
+        self.password = password
 
-    def __repr__(self):
-        return "%s(email=%r, userid=%r, notify=%r, auth=%r, **%r)" % (
-            self.__class__.__name__, self.email, self.userid,
-            self.notify, self._auth, self.notifications)
+        data = api.get_user(self.auth)
+        assert(self.email == data.email)  # this should never happen
+        self.userid = data.userid
 
-    def __str__(self):
-        return "<User %s:%s>" % (self.email, self.userid)
+        for key in self._fields:
+            setattr(self, key, getattr(data, key))
 
-    @classmethod
-    def connect(cls, email, password):
-        auth = (email, password)
-        info = api.get_user(auth)
-        return cls(auth=auth, **info)
+        self._artists = ArtistList(self.auth, self.userid)
 
     @property
     def artists(self):
-        if self._auth is None:
-            raise RuntimeError("can't query for unauthorized user")
-        if self._artists is None:
-            self._artists = [Artist(user=self, **x) for x in
-                             api.list_subscribed_artists(self._auth,
-                                                         self.userid)]
+        """artists are read-only"""
         return self._artists
+
+    def __repr__(self):
+        return "%s(email=%r, password='***')" % (self.__class__.__name__,
+                                                 self.email)
+
+    def __str__(self):
+        return "<muspy.com User %r>" % self.userid
+
+    @classmethod
+    def register(cls, email, password, send_activation):  # TODO: untested
+        api.create_user(email, password, send_activation)
+        return cls(email, password)
+
+    def delete(self):  # TODO: untested
+        api.delete_user(self.auth, self.userid)
+
+    def update(self):  # TOD: untested
+        web_data = api.get_user(self.auth, self.userid)
+        data = {k: getattr(self, k) for k in self._fields
+                if getattr(self, k) != getattr(web_data, k)}
+        return api.update_user(self.auth, self.userid, **data)
+
+
+class ArtistList(object):
+    def __init__(self, auth, userid):
+        self._auth = auth
+        self._userid = userid
+        self._data = [Artist(a) for a in
+                      api.list_artist_subscriptions(self._auth, self._userid)]
+
+    def __repr__(self):
+        return "ArtistList(%r)" % self._data
+
+    def __str__(self):
+        return "ArtistList(%s)" % self._data
+
+    @staticmethod
+    def _artist(other):
+        if isinstance(other, api.Artist):
+            return Artist(other)
+        elif isinstance(other, basestring):
+            return Artist(api.get_artist(other))
+        else:
+            raise ValueError("can't interpret %r" % other)
+
+    def __iadd__(self, other):
+        return self.add(other)
+
+    def __isub__(self, other):
+        return self.remove(other)
+
+    def add(self, other):  # TODO: untested
+        other = self._artist(other)
+        if other in self._data:
+            raise ValueError("%r already in list" % other)
+        api.add_artist_subscription(self._auth, self._userid, other.mbid)
+        return self._data.append(other)
+
+    def remove(self, other):  # TODO: untested
+        other = self._artist(other)
+        if other not in self._data:
+            raise ValueError("%r not in list" % other)
+        api.remove_artist_subscription(self._auth, self._userid, other.mbid)
+        self._data.remove(other)
+
+    def __getitem__(self, item):
+        return self._data.__getitem__(item)
+
+    def __len__(self):
+        return self._data.__len__()
+
+    def __contains__(self, other):
+        other = self._artist(other)
+        return other in self._data
+
+    def __iter__(self):
+        return iter(self._data)
+
+    iter = __iter__  # py2/py3 compatibility
 
 
 class Artist(object):
-    def __init__(self, user, name, sort_name, disambiguation, mbid):
-        self._user = user
-        self.name = name
-        self.disambiguation = disambiguation
-        self.sort_name = sort_name
-        self.mbid = mbid
+    def __init__(self, artist):
+        """
+        constructor
+
+        :param api.Artist artist: artist data
+        """
+        self._artist = artist
         self._releases = None
-
-    def __repr__(self):
-        return "%s(%r, %r, %r, %r, %r)" % (self.__class__.__name__, self._user,
-                                           self.name, self.sort_name,
-                                           self.disambiguation, self.mbid)
-
-    def __str__(self):
-        return '<Artist:%r>' % self.sort_name
 
     @property
     def releases(self):
         if self._releases is None:
-            data = api.get_all_releases_for_artist(self._user.userid, self.mbid)
-            self._releases = [Release(*row + (self,)) for row in data]
+            self._releases = api.list_all_releases_for_artist(self._artist.mbid)
         return self._releases
 
-
-class Release(object):
-    def __init__(self, name, type, date, mbid, artist, _artist=None):
-        self.name = name
-        self.type = type
-        self.date = date
-        self.mbid = mbid
-        self.artist = _artist if _artist else artist
+    def __str__(self):
+        return "<Artist %s>" % self._artist.name
 
     def __repr__(self):
-        return "%s(%r, %r, %r, %r, %r)" % (self.__class__.__name__, self.name,
-                                           self.type, self.date, self.mbid,
-                                           self.artist)
+        return "%s(%r)" % (self.__class__.__name__, self._artist)
 
-    def __str__(self):
-        return "<Release %r (%s)>" % (self.name, self.type)
+    def __getattr__(self, name):
+        return getattr(self._artist, name)
